200d1a6ca53ca0862f8f732f9a36ba4ae78670ec
zacharias.priller <contact@zachpriller.com>
2025-02-06 15:52:59 +0100

    Added static path to output.c

--- src/output.c
+++ src/output.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "output.h"
+#include "../include/output.h"
 #include "../include/filesystem.h"
 
 // Einfache Ausgabe der Dateinamen
